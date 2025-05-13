7682b0ef74742b9b32855e8c866733d1d7a3548e
Suriya-Kodehode <suriya.kodehode@gmail.com>
2025-05-13 19:05:34 +0200

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { base } from "./config"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: base,
   plugins: [react()],
 })
